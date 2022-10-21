2426a0a4ac380f502a5706a3b619ba30f0928a33
Andrew Tapper <andrew@tapper.codes>
2022-10-21 14:20:36 -0500

    :lipstick: remove nativewind, replace with twrnc

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw`flex-1 justify-center items-center bg-white`}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
