9bdefd6de743a2fa8254ae7863d5548b96a52a88
Andrew Tapper <andrew@tapper.codes>
2022-10-21 13:37:38 -0500

    :art: add components dir, add auth component

---
+++ src/components/Auth.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react'
+import { Alert, StyleSheet, View } from 'react-native'
+import { supabase } from '../lib/supabase'
+
+export default function Auth() {
+  return <></>
+}
