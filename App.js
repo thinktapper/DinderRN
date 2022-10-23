78180b789191af64721e03eb84fa23c8745b08e8
Andrew Tapper <andrew@tapper.codes>
2022-10-23 18:54:26 -0500

    :lipstick: add custom input and button components, add sign-in screen

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View } from 'react-native'
 import tw from 'twrnc'
-import StackNavigator from './src/lib/StackNavigator'
-import { Amplify, Auth } from 'aws-amplify'
-import { withAuthenticator } from 'aws-amplify-react-native'
-import config from './src/aws-exports'
+import SignInScreen from './src/screens/SignInScreen'
+// import StackNavigator from './src/lib/StackNavigator'
+// import { Amplify, Auth } from 'aws-amplify'
+// import { withAuthenticator } from 'aws-amplify-react-native'
+// import config from './src/aws-exports'
 
-Amplify.configure(config)
+// Amplify.configure(config)
 
 const App = () => {
   return (
-    <NavigationContainer>
-      {/* <AuthProvider> */}
-      <StackNavigator />
-      {/* </AuthProvider> */}
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <StackNavigator />
+    // </NavigationContainer>
+
+    <SafeAreaView style={tw`flex-1 bg-slate-200`}>
+      <SignInScreen />
+    </SafeAreaView>
   )
 }
 
-export default withAuthenticator(App)
+// export default withAuthenticator(App)
+export default App
---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,63 @@
+import {
+  View,
+  Text,
+  Image,
+  useWindowDimensions,
+  ScrollView,
+} from 'react-native'
+import React, { useState } from 'react'
+import tw from 'twrnc'
+import Logo from '../../../assets/images/dinder-double_flame-black.png'
+import CustomInput from '../../components/CustomInput'
+import CustomButton from '../../components/CustomButton'
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { height } = useWindowDimensions()
+
+  const onSignInPressed = () => {
+    console.warn('Sign In Pressed')
+  }
+
+  const onSignUpPressed = () => {
+    console.warn('Sign Up Pressed')
+  }
+
+  return (
+    <ScrollView>
+      <View style={tw`items-center p-5`}>
+        <Image
+          source={Logo}
+          style={[tw`max-w-xs max-h-56`, { height: height * 0.3 }]}
+          resizeMode="contain"
+        />
+
+        <CustomInput placeholder="Email" value={email} setValue={setEmail} />
+        <CustomInput
+          placeholder="Password"
+          value={password}
+          setValue={setPassword}
+          secureTextEntry
+        />
+
+        <CustomButton text="Sign In" onPress={onSignInPressed} />
+
+        <CustomButton
+          text="Forgot password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
+
+        <CustomButton
+          text="Don't have an account? Create one"
+          onPress={onSignUpPress}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  )
+}
+
+export default SignInScreen
