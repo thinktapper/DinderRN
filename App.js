572d8e19411b4b5d5e0bc2b80becf44d17958a4f
Andrew Tapper <andrew@tapper.codes>
2022-11-15 23:34:53 -0600

    :passport_control: create authContext provider configured with supabase auth

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 // import 'react-native-gesture-handler'
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView } from 'react-native'
-import tw from 'twrnc'
+import React from 'react'
+import 'react-native-url-polyfill/auto'
+// import { StatusBar } from 'expo-status-bar'
+// import { SafeAreaView } from 'react-native'
+// import tw from 'twrnc'
 import Navigation from './src/navigation'
-import { Amplify } from 'aws-amplify'
-import config from './src/aws-exports'
+
 import { AppProvider } from './src/context/AppProvider'
 import { AuthProvider } from './src/context/AuthProvider'
-
-Amplify.configure({
-  ...config,
-  Analytics: {
-    disabled: true,
-  },
-})
 
 const App = () => {
   return (
     // <SafeAreaView style={tw`flex-1 bg-slate-200`}>
-    <AuthProvider>
-      <AppProvider>
+    <AppProvider>
+      <AuthProvider>
         <Navigation />
-      </AppProvider>
-    </AuthProvider>
+      </AuthProvider>
+    </AppProvider>
     // </SafeAreaView>
   )
 }
--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,111 +1,37 @@
-import React, {
-  createContext,
-  useContext,
-  useCallback,
-  useState,
-  useEffect,
-} from 'react'
-import { Auth, Hub, DataStore } from 'aws-amplify'
-import { User } from '../models/'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Alert } from 'react-native'
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { supabase } from '../utils/supabase'
+import { Session } from '@supabase/supabase-js'
+// import AsyncStorage from '@react-native-async-storage/async-storage'
+// import { Alert } from 'react-native'
 
-const AuthContext = createContext({})
+const AuthContext = createContext({
+  user: null,
+  session: Session,
+})
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null)
-  const [userLoading, setUserLoading] = useState(true)
+  const [session, setSession] = useState(null)
 
   useEffect(() => {
-    // Create listener
-    const listener = Hub.listen('datastore', async hubData => {
-      const { event, data } = hubData.payload
-      if (event === 'modelSynced' && data?.model?.name === 'User') {
-        console.log('User Model has finished syncing')
-        setUserLoading(false)
-      }
-    })
-
-    return () => listener()
-  }, [])
-
-  // useEffect(() => {
-  //   const getCurrentUser = async () => {
-  //     const authUser = await Auth.currentAuthenticatedUser()
-
-  //     const dbUsers = await DataStore.query(User, u =>
-  //       u.sub('eq', authUser.attributes.sub),
-  //     )
-
-  //     if (!dbUsers || dbUsers.length === 0) {
-  //       console.warn('This is a new user')
-  //       return
-  //     }
-  //     const dbUser = dbUsers[0]
-  //     setUser(dbUser)
-  //     // setName(dbUser.name)
-  //     // setBio(dbUser.bio)
-  //   }
-  //   getCurrentUser()
-  // }, [])
-
-  const handleSignIn = useCallback(async data => {
-    // if (userLoading) {
-    //   return
-    // }
-
-    setUserLoading(true)
-    try {
-      const response = await Auth.signIn(data.username, data.password)
-      // console.log(response)
-      return response
-    } catch (error) {
-      Alert.alert('Oops!', error.message)
+    // Set user and session from storage
+    const session = supabase.auth.session()
+    setSession(session)
+    setUser(!!session)
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        console.log(`Supabase auth event: ${event}`)
+        setSession(session)
+        setUser(!!session)
+      },
+    )
+    return () => {
+      authListener.unsubscribe()
     }
-    setUserLoading(false)
-  }, [])
-
-  const handleSignOut = async () => {
-    await DataStore.clear()
-    Auth.signOut()
-      .then(() => {
-        setUser(null)
-        Alert.alert('Signed Out')
-      })
-      .catch(err => console.log('Error signing out: ', err))
-  }
-
-  // Check user authentication status
-  // const checkUser = useCallback(async () => {
-  //   try {
-  //     const authUser = await Auth.currentAuthenticatedUser({
-  //       bypassCache: true,
-  //     })
-  //     if (authUser) {
-  //       setUser(authUser)
-  //     }
-  //   } catch (error) {
-  //     setUser(null)
-  //   }
-  // }, [])
-
-  // useEffect(() => {
-  //   checkUser()
-  // }, [])
-
-  // useEffect(() => {
-  //   const listener = data => {
-  //     if (data.payload.event === 'signIn' || data.payload.event === 'signOut') {
-  //       checkUser()
-  //     }
-  //   }
-  //   Hub.listen('auth', listener)
-  //   return () => Hub.remove('auth', listener)
-  // }, [])
+  }, [user])
 
   return (
-    <AuthContext.Provider
-      value={{ user, userLoading, handleSignIn, handleSignOut }}>
+    <AuthContext.Provider value={{ user, session }}>
       {children}
     </AuthContext.Provider>
   )
