270276261615791d3bd416f7e608c140a18f296f
Andrew Tapper <andrew@tapper.codes>
2022-11-15 22:36:28 -0600

    :ambulance: begin migration to supabase backend

---
+++ src/utils/supabase.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { SUPABASE_URL, SUPABASE_KEY } from '@env'
+
+const supabaseUrl = `${SUPABASE_URL}`
+const supabaseAnonKey = `${SUPABASE_KEY}`
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
